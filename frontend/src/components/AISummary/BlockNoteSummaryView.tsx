8cf538480d8d54ed33c5e9e6f2ba0ef6fa1c1631
sujithatzackriya <sujith@zackriya.com>
2025-10-21 14:00:32 +0530

    Changed the UI behaviour : blovknotes summary

--- frontend/src/components/AISummary/BlockNoteSummaryView.tsx
+++ frontend/src/components/AISummary/BlockNoteSummaryView.tsx
@@ -219,8 +219,8 @@ export const BlockNoteSummaryView = forwardRef<BlockNoteSummaryViewRef, BlockNot
   if (format === 'blocknote') {
     console.log('🎨 Rendering BLOCKNOTE format (direct)');
     return (
-      <div className="flex flex-col h-full">
-        <div className="flex-1 overflow-auto">
+      <div className="flex flex-col w-full">
+        <div className="w-full">
           <Editor
             initialContent={data.summary_json}
             onChange={(blocks) => {
@@ -238,8 +238,8 @@ export const BlockNoteSummaryView = forwardRef<BlockNoteSummaryViewRef, BlockNot
   if (format === 'markdown') {
     console.log('🎨 Rendering MARKDOWN format (parsed to BlockNote)');
     return (
-      <div className="flex flex-col h-full">
-        <div className="flex-1 overflow-auto">
+      <div className="flex flex-col w-full">
+        <div className="w-full">
           <BlockNoteView
             editor={editor}
             editable={true}
